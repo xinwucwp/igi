36afa51ce7736e3a608b6b99820c20f923faf001
xinwucwp <xinwucwp@gmail.com>
2018-01-11 00:33:46 -0600

    First commit

---
+++ src/igi/FastImageGuidedInterp.java
@@ -0,0 +1,479 @@
+package igi;
+
+import edu.mines.jtk.dsp.*;
+import edu.mines.jtk.util.*;
+import static edu.mines.jtk.util.ArrayMath.*;
+
+/**
+ * Fast image-guided interpolation across faults by solving 
+ * weighted anisotropic harmonic (Gaussian operator) 
+ * and bi-harmonic (Laplacian operator) interpolation.
+ * @author Xinming Wu
+ * @version 2015.06.07
+ */
+public class FastImageGuidedInterp {
+
+  /**
+   * Constructs an interpolator.
+   * @param fx know values at the known points.
+   * @param x1 1st coordinates of known points.
+   * @param x2 2nd coordinates of known points.
+   */
+  public FastImageGuidedInterp(
+    float[] fx, float[] x1, float[] x2) 
+  {
+    _fx = copy(fx);
+    _x1 = copy(x1);
+    _x2 = copy(x2);
+  }
+
+  /**
+   * Constructs an interpolator.
+   * @param fx know values at the known points.
+   * @param x1 1st coordinates of known points.
+   * @param x2 2nd coordinates of known points.
+   * @param x3 3rd coordinates of known points.
+   */
+  public FastImageGuidedInterp(
+    float[] fx, float[] x1, float[] x2, float[] x3) 
+  {
+    _fx = copy(fx);
+    _x1 = copy(x1);
+    _x2 = copy(x2);
+    if(x3!=null){_x3=copy(x3);}
+  }
+
+  /**
+   * Set smoothing for preconditioning in a CG solver.
+   * @param sigma1 smoother half-width for 1st dimension.
+   * @param sigma2 smoother half-width for 2nd dimension.
+   */
+  public void setSmoothings(double sigma1, double sigma2) {
+    _sigma1 = (float)sigma1;
+    _sigma2 = (float)sigma2;
+  }
+
+  /**
+   * Set iterations for a CG solver.
+   * @param niter number of the maximum iterations.
+   */
+  public void setIters(int niter) {
+    _niter =  niter;
+  }
+
+  /**
+   * Set 2D structure tensors for 2D image-guided interpolation.
+   * @param d2 2D structure tensor field.
+   */
+  public void setTensors(Tensors2 d2) {
+    _d2 = d2;
+  }
+
+  /**
+   * Set 3D structure tensors for 3D image-guided interpolation.
+   * @param d3 3D structure tensor field.
+   */
+  public void setTensors(Tensors3 d3) {
+    _d3 = d3;
+  }
+
+  /**
+   * Apply for 2D image-guided interpolation.
+   * @param s1 sampling in the 1st (vertical) dimension.
+   * @param s2 sampling in the 2nd (lateral) dimension.
+   * @return array of the interpolated image.
+   */
+  public float[][] grid(Sampling s1, Sampling s2) {
+    int n1 = s1.getCount();
+    int n2 = s2.getCount();
+    float[][] r = new float[n2][n1];
+    float[][] wp = fillfloat(1f,n1,n2);
+    int np = _x1.length;
+    for (int ip=0; ip<np; ++ip) {
+      _x1[ip] = (float)s1.indexOfNearest(_x1[ip]);
+      _x2[ip] = (float)s2.indexOfNearest(_x2[ip]);
+    }
+    float[][] b = new float[n2][n1];
+    setInitial(r);
+    VecArrayFloat2 vb = new VecArrayFloat2(b);
+    VecArrayFloat2 vr = new VecArrayFloat2(r);
+    Smoother2 sm2 = new Smoother2(_sigma1,_sigma2,wp);
+    CgSolver cg = new CgSolver(_small,_niter);
+    A2 a2 = new A2(_d2,wp);
+    M2 m2 = new M2(_x1,_x2,sm2);
+    vb.zero();
+    cg.solve(a2,m2,vb,vr);
+    return r;
+  }
+
+  /**
+   * Apply for 2D image-guided interpolation.
+   * @param s1 sampling in the 1st (vertical) dimension.
+   * @param s2 sampling in the 2nd (lateral) dimension.
+   * @return array of the interpolated image.
+   */
+  public float[][] grid(Sampling s1, Sampling s2, float[][] wp) {
+    int n1 = s1.getCount();
+    int n2 = s2.getCount();
+    float[][] r = new float[n2][n1];
+    int np = _x1.length;
+    for (int ip=0; ip<np; ++ip) {
+      _x1[ip] = (float)s1.indexOfNearest(_x1[ip]);
+      _x2[ip] = (float)s2.indexOfNearest(_x2[ip]);
+    }
+    float[][] b = new float[n2][n1];
+    setInitial(r);
+    VecArrayFloat2 vb = new VecArrayFloat2(b);
+    VecArrayFloat2 vr = new VecArrayFloat2(r);
+    Smoother2 sm2 = new Smoother2(_sigma1,_sigma2,null);
+    CgSolver cg = new CgSolver(_small,_niter);
+    A2 a2 = new A2(_d2,wp);
+    M2 m2 = new M2(_x1,_x2,sm2);
+    vb.zero();
+    cg.solve(a2,m2,vb,vr);
+    return r;
+  }
+
+
+  /**
+   * Apply for 3D image-guided interpolation.
+   * @param sp screen points on faults.
+   * @param wp weights, zeros on faults, ones elsewhere.
+   * @return array of the interpolated image.
+   */
+  public float[][][] apply(
+    float[][][] sp, float[][][] wp) {
+    int n3 = wp.length;
+    int n2 = wp[0].length;
+    int n1 = wp[0][0].length;
+    float[][][] b = new float[n3][n2][n1];
+    float[][][] r = new float[n3][n2][n1];
+    setInitial(r);
+    VecArrayFloat3 vr = new VecArrayFloat3(r);
+    VecArrayFloat3 vb = new VecArrayFloat3(b);
+    Smoother3 s3 = new Smoother3(_sigma1,_sigma2,_sigma2,wp);
+    CgSolver cg = new CgSolver(_small,_niter);
+    A3 a3 = new A3(_d3,sp,wp);
+    M3 m3 = new M3(_x1,_x2,_x3,s3);
+    vb.zero();
+    cg.solve(a3,m3,vb,vr);
+    return r;
+  }
+
+  private void setInitial(float[][] x) {
+    if(_x1==null||_x2==null||_fx==null){return;}
+    int np = _x1.length;
+    for (int ip=0; ip<np; ++ip) {
+      int i1 = (int)_x1[ip];
+      int i2 = (int)_x2[ip];
+      x[i2][i1] = _fx[ip];
+    }
+  }
+
+  private void setInitial(float[][][] x) {
+    if(_x1==null||_x2==null||_x3==null||_fx==null){return;}
+    int np = _x1.length;
+    for (int ip=0; ip<np; ++ip) {
+      int i1 = (int)_x1[ip];
+      int i2 = (int)_x2[ip];
+      int i3 = (int)_x3[ip];
+      x[i3][i2][i1] = _fx[ip];
+    }
+  }
+
+  ///////////////////////////////////////////////////////////////////////////
+  // private
+  private Tensors2 _d2;
+  private Tensors3 _d3;
+
+  private static float[] _x1 = null; // 1st coordinates of the known points
+  private static float[] _x2 = null; // 2nd coordinates of the known points
+  private static float[] _x3 = null; // 3rd coordinates of the known points
+  private static float[] _fx = null; // known values at the known points
+  private float _sigma1 = 10.0f; // half-width of smoother in 1st dimension
+  private float _sigma2 = 10.0f; // half-width of smoother in 2nd dimension
+  private float _small = 0.010f; // stop CG iterations if residuals are small
+  private int _niter = 800; // maximum number of inner CG iterations
+
+  private static class A2 implements CgSolver.A {
+    A2(Tensors2 et, float[][] wp) 
+    {
+      _et = et;
+      _wp = wp;
+    }
+    public void apply(Vec vx, Vec vy) {
+      VecArrayFloat2 v2x = (VecArrayFloat2)vx;
+      VecArrayFloat2 v2y = (VecArrayFloat2)vy;
+      float[][] x = v2x.getArray();
+      float[][] y = v2y.getArray();
+      float[][] z = copy(x);
+      float[][] t = copy(x);
+      VecArrayFloat2 v2t = new VecArrayFloat2(t);
+      v2y.zero();
+      v2t.zero();
+      applyLhs(_et,_wp,z,y);
+      applyLhs(_et,_wp,y,t);
+      v2y.add(1.f,v2t,100f);
+    }
+
+    private float[][] _wp=null;
+    private Tensors2 _et = null;
+  }
+
+  // Preconditioner; includes smoothers and constraints.
+  private static class M2 implements CgSolver.A {
+    M2(float[] x1, float[] x2, Smoother2 s2) {
+      _x1 = x1;
+      _x2 = x2;
+      _s2 = s2;
+    }
+    public void apply(Vec vx, Vec vy) {
+      VecArrayFloat2 v2x = (VecArrayFloat2)vx;
+      VecArrayFloat2 v2y = (VecArrayFloat2)vy;
+      float[][] x = v2x.getArray();
+      float[][] y = v2y.getArray();
+      copy(x,y);
+      constrain(_x1,_x2,y);
+      _s2.apply(y);
+      constrain(_x1,_x2,y);
+    }
+    private Smoother2 _s2;
+    private float[] _x1,_x2;
+  }
+
+  private static class A3 implements CgSolver.A {
+    A3(Tensors3 et, float[][][] sp, float[][][] wp) 
+    {
+      _et = et;
+      _sp = sp;
+      _wp = wp;
+    }
+    public void apply(Vec vx, Vec vy) {
+      VecArrayFloat3 v3x = (VecArrayFloat3)vx;
+      VecArrayFloat3 v3y = (VecArrayFloat3)vy;
+      float[][][] x = v3x.getArray();
+      float[][][] y = v3y.getArray();
+      float[][][] z = copy(x);
+      float[][][] t = copy(x);
+      VecArrayFloat3 v3t = new VecArrayFloat3(t);
+      v3y.zero();
+      v3t.zero();
+      applyLhs(_et,_wp,z,y);
+      applyLhs(_et,_wp,y,t);
+      v3y.add(1.f,v3t,50f);
+      if(_sp!=null) {
+        screenLhs(_sp[0],_sp[1],_sp[3][0],z,y);
+      }
+    }
+
+    private Tensors3 _et = null;
+    private float[][][] _wp=null;
+    private float[][][] _sp=null;
+  }
+
+  // Preconditioner; includes smoothers and constraints.
+  private static class M3 implements CgSolver.A {
+    M3(float[] x1, float[] x2, float[] x3, Smoother3 s3) {
+      _x1 = x1;
+      _x2 = x2;
+      _x3 = x3;
+      _s3 = s3;
+    }
+    public void apply(Vec vx, Vec vy) {
+      VecArrayFloat3 v3x = (VecArrayFloat3)vx;
+      VecArrayFloat3 v3y = (VecArrayFloat3)vy;
+      float[][][] x = v3x.getArray();
+      float[][][] y = v3y.getArray();
+      copy(x,y);
+      constrain(_x1,_x2,_x3,y);
+      _s3.apply(y);
+      constrain(_x1,_x2,_x3,y);
+    }
+    private Smoother3 _s3;
+    private float[] _x1,_x2,_x3;
+  }
+
+  private static void constrain(
+    float[] x1, float[] x2, float[][] x) 
+  {
+    if (x1!=null && x2!=null) {
+      int np = x1.length;
+      for (int ip=0; ip<np; ++ip) {
+        int i1 = (int)x1[ip]; 
+        int i2 = (int)x2[ip]; 
+        x[i2][i1] = 0.0f;
+      }
+    }
+  }
+
+  private static void constrain(
+    float[] x1, float[] x2, float[] x3, float[][][] x) 
+  {
+    if (x1!=null && x2!=null && x3!=null) {
+      int np = x1.length;
+      for (int ip=0; ip<np; ++ip) {
+        int i1 = (int)x1[ip]; 
+        int i2 = (int)x2[ip]; 
+        int i3 = (int)x3[ip]; 
+        x[i3][i2][i1] = 0.0f;
+      }
+    }
+  }
+
+
+  private void makeRhs(float[][] r) {
+    int np = _x1.length;
+    for (int ip=0; ip<np; ++ip) {
+      int i1 = (int)_x1[ip];
+      int i2 = (int)_x2[ip];
+      r[i2][i1] = _fx[ip];
+    }
+
+  }
+
+  private static void applyLhs(
+    final Tensors2 d, final float[][] wp, 
+    final float[][] x, final float[][] y)
+  {
+    zero(y);
+    int n2 = x.length;
+    int n1 = x[0].length;
+    float[] ds = fillfloat(1.0f,3);
+    ds[0] = 1.0f;
+    ds[1] = 0.0f;
+    ds[2] = 1.0f;
+    for (int i2=1; i2<n2; ++i2) {
+      for (int i1=1; i1<n1; ++i1) {
+        if(d!=null){d.getTensor(i1,i2,ds);}
+        float wpi = (wp!=null)?wp[i2][i1]:1.0f;
+        float wps = wpi*wpi;
+        float d11 = ds[0]*wps;
+        float d12 = ds[1]*wps;
+        float d22 = ds[2]*wps;
+        float xa = 0.0f;
+        float xb = 0.0f;
+        xa += x[i2  ][i1  ];
+        xb -= x[i2  ][i1-1];
+        xb += x[i2-1][i1  ];
+        xa -= x[i2-1][i1-1];
+        float x1 = 0.5f*(xa+xb);
+        float x2 = 0.5f*(xa-xb);
+        float y1 = d11*x1+d12*x2;
+        float y2 = d12*x1+d22*x2;
+        float ya = 0.5f*(y1+y2);
+        float yb = 0.5f*(y1-y2);
+        y[i2  ][i1  ] += ya;
+        y[i2  ][i1-1] -= yb;
+        y[i2-1][i1  ] += yb;
+        y[i2-1][i1-1] -= ya;
+      }
+    }
+
+  }
+
+  private static void applyLhs(
+    final Tensors3 d, final float[][][] wp, 
+    final float[][][] x, final float[][][] y)
+  { 
+    final int n3 = y.length;
+    Parallel.loop(1,n3,2,new Parallel.LoopInt() {
+    public void compute(int i3) {
+      applyLhsSlice3(i3,d,wp,x,y);
+    }});
+    Parallel.loop(2,n3,2,new Parallel.LoopInt() {
+    public void compute(int i3) {
+      applyLhsSlice3(i3,d,wp,x,y);
+    }});
+  }
+
+  private static void screenLhs(
+    float[][] cp, float[][] cm, float[] fl, float[][][] x, float[][][] y) 
+  {
+    int nc = cp[0].length;
+    for (int ic=0; ic<nc; ++ic) {
+      int i1p = (int)cp[0][ic];
+      int i2p = (int)cp[1][ic];
+      int i3p = (int)cp[2][ic];
+      int i1m = (int)cm[0][ic];
+      int i2m = (int)cm[1][ic];
+      int i3m = (int)cm[2][ic];
+      //float fls = fl[ic];//*fl[ic];
+
+      float dx = 0.0f;
+
+      dx += x[i3p][i2p][i1p];
+      dx -= x[i3m][i2m][i1m];
+
+      //dx *= fls;
+
+      y[i3m][i2m][i1m] -= dx;
+      y[i3p][i2p][i1p] += dx;
+    }
+  }
+
+  // 3D LHS
+  private static void applyLhsSlice3(
+    int i3, Tensors3 d, float[][][] wp, float[][][] x, float[][][] y)
+  {
+    int n2 = y[0].length;
+    int n1 = y[0][0].length;
+    float[] di = fillfloat(1.0f,6);
+    for (int i2=1; i2<n2; ++i2) {
+      float[] x00 = x[i3  ][i2  ];
+      float[] x01 = x[i3  ][i2-1];
+      float[] x10 = x[i3-1][i2  ];
+      float[] x11 = x[i3-1][i2-1];
+      float[] y00 = y[i3  ][i2  ];
+      float[] y01 = y[i3  ][i2-1];
+      float[] y10 = y[i3-1][i2  ];
+      float[] y11 = y[i3-1][i2-1];
+      for (int i1=1,i1m=0; i1<n1; ++i1,++i1m) {
+        if(d!=null){d.getTensor(i1,i2,i3,di);}
+        float wpi = (wp!=null)?wp[i3][i2][i1]:1.0f;
+        float wps = wpi*wpi;
+        float d11 = di[0];
+        float d12 = di[1];
+        float d13 = di[2];
+        float d22 = di[3];
+        float d23 = di[4];
+        float d33 = di[5];
+        float xa = 0.0f;
+        float xb = 0.0f;
+        float xc = 0.0f;
+        float xd = 0.0f;
+
+        xa += x00[i1 ];
+        xd -= x00[i1m];
+        xb += x01[i1 ];
+        xc -= x01[i1m];
+        xc += x10[i1 ];
+        xb -= x10[i1m];
+        xd += x11[i1 ];
+        xa -= x11[i1m];
+
+        float x1 = 0.25f*(xa+xb+xc+xd)*wps;
+        float x2 = 0.25f*(xa-xb+xc-xd)*wps;
+        float x3 = 0.25f*(xa+xb-xc-xd)*wps;
+
+        float y1 = d11*x1+d12*x2+d13*x3;
+        float y2 = d12*x1+d22*x2+d23*x3;
+        float y3 = d13*x1+d23*x2+d33*x3;
+
+        float ya = 0.25f*(y1+y2+y3);
+        float yb = 0.25f*(y1-y2+y3);
+        float yc = 0.25f*(y1+y2-y3);
+        float yd = 0.25f*(y1-y2-y3);
+
+        y00[i1 ] += ya;
+        y00[i1m] -= yd;
+        y01[i1 ] += yb;
+        y01[i1m] -= yc;
+        y10[i1 ] += yc;
+        y10[i1m] -= yb;
+        y11[i1 ] += yd;
+        y11[i1m] -= ya;  
+      }
+    }
+  }
+
+}
---
+++ src/igi/demo2.py
@@ -0,0 +1,369 @@
+import sys
+from java.awt import *
+from java.lang import *
+from java.util import *
+from javax.swing import *
+
+from edu.mines.jtk.io import *
+from edu.mines.jtk.dsp import *
+from edu.mines.jtk.awt import *
+from edu.mines.jtk.sgl import *
+from edu.mines.jtk.mosaic import *
+from edu.mines.jtk.interp import *
+from edu.mines.jtk.util.ArrayMath import *
+
+
+from igi import *
+
+pngDir = None
+pngDir = "../../png/"
+def main(args):
+  goDemoNotreDame() 
+  goDemoTeapot()
+
+#a demo of 2D isotropic interpolation
+def goDemoNotreDame():
+  x,y,f = getDataNotreDame()
+  sx,sy = getSamplingsNotreDame(grid="fine")
+  fx = putDataOnGrid(f,x,y,sx,sy)
+  #set tensor=None to construct an isotropic interplator 
+  igi = FastImageGuidedInterp(f,x,y,None) 
+  igi.setSmoothings(10.0,10.0)
+  igi.setIters(100)
+  g = igi.grid(sx,sy)
+  plot2(f,x,y,fx,sx,sy,title="notreDame known points",
+          contours=False,points=False, png="notreDameKnownPoints")
+  plot2(f,x,y,g,sx,sy,title="notreDame interpolation",png="notreDameInterp")
+  plotSurface(f,x,y,g,sx,sy,png="notreDameSurface")
+
+def goDemoTeapot():
+  t,x,f = getDataTeapot()
+  st,sx,s = getImageTeapot()
+  lof = LocalOrientFilter(4,2)
+  et = makeImageTensors(s)
+  #et = lof.applyForTensors(s) # structure tensors
+  ch = coherence(3,et,s) # structure-oriented coherence c
+  et.setEigenvalues(0.001,1.0)
+  igi = FastImageGuidedInterp(f,t,x,None) 
+  igi.setSmoothings(10.0,10.0)
+  igi.setIters(200)
+  igi.setTensors(et)
+  ch = pow(ch,8)
+  fg = igi.grid(st,sx,ch)
+  plot2Teapot(f,t,x,s,st,sx,None,"Known value","tp2f")
+  plot2Teapot(f,t,x,s,st,sx,fg,"Guided harmonic","tp2q")
+
+
+def getDataTeapot():
+  """
+  Values set interactively by Dave Hale.
+  """
+  txf = [
+    30, 69,0.50,  99, 72,0.50, 153, 69,0.50, 198, 68,0.50, 
+    63, 71,0.90, 128, 72,0.90, 176, 69,0.90,
+    29,172,0.35,  97,173,0.35, 150,173,0.35, 192,176,0.35,
+    63,173,0.75, 127,174,0.75, 172,174,0.75,
+    33,272,0.20, 103,270,0.20, 160,267,0.20, 199,267,0.20,
+    70,271,0.60, 134,268,0.60, 179,267,0.60]
+  n = len(txf)/3
+  t = zerofloat(n)
+  x = zerofloat(n)
+  f = zerofloat(n)
+  copy(n,0,3,txf,0,1,t)
+  copy(n,1,3,txf,0,1,x)
+  copy(n,2,3,txf,0,1,f)
+  #t = add(0.5,mul(0.004,t))
+  #x = add(0.0,mul(0.025,x))
+  return t,x,f
+
+def getImageTeapot():
+  #ft,fx = 0.500,0.000
+  #dt,dx = 0.004,0.025
+  ft,fx = 0.0,0.0
+  dt,dx = 1.0,1.0
+  nt,nx = 251,357
+  image = zerofloat(nt,nx)
+  st,sx = Sampling(nt,dt,ft),Sampling(nx,dx,fx)
+  ais = ArrayInputStream("../../data/tp73.dat")
+  ais.readFloats(image)
+  ais.close()
+  return st,sx,image
+
+def makeImageTensors(s):
+  """ 
+  Returns tensors for guiding along features in specified image.
+  """
+  sigma = 3
+  n1,n2 = len(s[0]),len(s)
+  lof = LocalOrientFilter(sigma)
+  t = lof.applyForTensors(s) # structure tensors
+  c = coherence(sigma,t,s) # structure-oriented coherence c
+  c = clip(0.0,0.99,c) # c clipped to range [0,1)
+  t.scale(sub(1.0,c)) # scale structure tensors by 1-c
+  t.invertStructure(1.0,1.0) # invert and normalize
+  return t
+
+def coherence(sigma,t,s):
+  lsf = LocalSemblanceFilter(sigma,4*sigma)
+  return lsf.semblance(LocalSemblanceFilter.Direction2.V,t,s)
+
+
+def putDataOnGrid(f,x,y,sx,sy):
+  """ facilitates comparison of different gridders """
+  n = len(f)
+  ny,nx = sy.count,sx.count
+  fx = zerofloat(ny,nx)
+  for i in range(n):
+    ix = sx.indexOfNearest(x[i])
+    iy = sy.indexOfNearest(y[i])
+    x[i] = sx.getValue(ix)
+    y[i] = sy.getValue(iy)
+    fx[iy][ix] = f[i]
+    fx[iy+1][ix] = f[i]
+    fx[iy-1][ix] = f[i]
+    fx[iy][ix-1] = f[i]
+    fx[iy][ix+1] = f[i]
+    fx[iy-1][ix-1] = f[i]
+    fx[iy-1][ix+1] = f[i]
+    fx[iy+1][ix-1] = f[i]
+    fx[iy+1][ix+1] = f[i]
+  return fx
+
+def getSamplingsNotreDame(grid="fine"):
+  fx,fy = -5.0,-5.0
+  dx,dy = 2.00,2.00; nx,ny = 165,165
+  if grid=="coarser": dx,dy = 8.00,8.00; nx,ny = 42,42
+  elif grid=="coarse": dx,dy = 4.00,4.00; nx,ny = 83,83
+  elif grid=="medium": dx,dy = 2.00,2.00; nx,ny = 165,165
+  elif grid=="fine": dx,dy = 1.00,1.00; nx,ny = 329,329
+  elif grid=="finer": dx,dy = 0.50,0.50; nx,ny = 657,657
+  elif grid=="finest": dx,dy = 0.25,0.25; nx,ny = 1313,1313
+  sx,sy = Sampling(nx,dx,fx),Sampling(ny,dy,fy)
+  return sx,sy
+
+def getDataNotreDame():
+  """
+  Elevation data from Davis, J.C., 2002, Statistics and Data Analysis in
+  Geology, 3rd Edition: Wiley, page 374. The name of the file from which 
+  these data were copied is NOTREDAM.TXT. These data were collected over
+  a small area by students in a surveying class.
+  These data are used in Figure 3 of Wessel, P., 2009, A general-purpose 
+  Green's function-based interpolator: Computers and Geosciences 35, 
+  1247-1254.
+  This function returns three float arrays x, y, z containing Eastings,
+  Northings and elevations. Units are unknown. Davis writes that one 
+  map unit = 50 ft, but this implies extremely steep terrain, with 
+  slopes exceeding 45 degrees.
+  """
+  xyzNotreDame = [
+  0.3, 6.1, 870.0, 1.4, 6.2, 793.0, 2.4, 6.1, 755.0, 3.6, 6.2, 690.0,
+  5.7, 6.2, 800.0, 1.6, 5.2, 800.0, 2.9, 5.1, 730.0, 3.4, 5.3, 728.0,
+  3.4, 5.7, 710.0, 4.8, 5.6, 780.0, 5.3, 5.0, 804.0, 6.2, 5.2, 855.0,
+  0.2, 4.3, 830.0, 0.9, 4.2, 813.0, 2.3, 4.8, 762.0, 2.5, 4.5, 765.0,
+  3.0, 4.5, 740.0, 3.5, 4.5, 765.0, 4.1, 4.6, 760.0, 4.9, 4.2, 790.0,
+  6.3, 4.3, 820.0, 0.9, 3.2, 855.0, 1.7, 3.8, 812.0, 2.4, 3.8, 773.0,
+  3.7, 3.5, 812.0, 4.5, 3.2, 827.0, 5.2, 3.2, 805.0, 6.3, 3.4, 840.0,
+  0.3, 2.4, 890.0, 2.0, 2.7, 820.0, 3.8, 2.3, 873.0, 6.3, 2.2, 875.0,
+  0.6, 1.7, 873.0, 1.5, 1.8, 865.0, 2.1, 1.8, 841.0, 2.1, 1.1, 862.0,
+  3.1, 1.1, 908.0, 4.5, 1.8, 855.0, 5.5, 1.7, 850.0, 5.7, 1.0, 882.0,
+  6.2, 1.0, 910.0, 0.4, 0.5, 940.0, 1.4, 0.6, 915.0, 1.4, 0.1, 890.0,
+  2.1, 0.7, 880.0, 2.3, 0.3, 870.0, 3.1, 0.0, 880.0, 4.1, 0.8, 960.0,
+  5.4, 0.4, 890.0, 6.0, 0.1, 860.0, 5.7, 3.0, 830.0, 3.6, 6.0, 705.0]
+  nNotreDame = len(xyzNotreDame)/3
+  xNotreDame = zerofloat(nNotreDame)
+  yNotreDame = zerofloat(nNotreDame)
+  zNotreDame = zerofloat(nNotreDame)
+  copy(nNotreDame,0,3,xyzNotreDame,0,1,xNotreDame)
+  copy(nNotreDame,1,3,xyzNotreDame,0,1,yNotreDame)
+  copy(nNotreDame,2,3,xyzNotreDame,0,1,zNotreDame)
+  xNotreDame = mul(50.0,xNotreDame) # Davis says one map unit = 50 ft,
+  yNotreDame = mul(50.0,yNotreDame) # but this makes the terrain steep!
+  return xNotreDame,yNotreDame,zNotreDame
+
+#############################################################################
+# plotting
+backgroundColor = Color.WHITE
+cjet = ColorMap.JET
+alpha = fillfloat(1.0,256); alpha[0] = 0.0
+ajet = ColorMap.setAlpha(cjet,alpha)
+
+def plot2Teapot(f,x1,x2,s,s1,s2,g=None,label=None,png=None,et=None):
+  n1 = len(s[0])
+  n2 = len(s)
+  panel = panel2Teapot()
+  #panel.setHInterval(2.0)
+  #panel.setVInterval(0.2)
+  #panel.setHLabel("Distance (km)")
+  #panel.setVLabel("Time (s)")
+  #panel.setHInterval(100.0)
+  #panel.setVInterval(100.0)
+  #panel.setHLabel("Pixel")
+  #panel.setVLabel("Pixel")
+  if label:
+    panel.addColorBar(label)
+  else:
+    panel.addColorBar()
+  panel.setColorBarWidthMinimum(180)
+  pv = panel.addPixels(s1,s2,s)
+  pv.setInterpolation(PixelsView.Interpolation.LINEAR)
+  pv.setColorModel(ColorMap.GRAY)
+  pv.setClips(-4.5,4.5)
+  if g:
+    alpha = 0.5
+  else:
+    g = zerofloat(s1.count,s2.count)
+    alpha = 0.0
+  pv = panel.addPixels(s1,s2,g)
+  pv.setInterpolation(PixelsView.Interpolation.LINEAR)
+  pv.setColorModel(ColorMap.getJet(alpha))
+  if label and label[0]=="T":
+    pv.setClips(0.0,1000.0)
+  else:
+    pv.setClips(0.0,1.0)
+  cmap = pv.getColorMap()
+  if et:
+    tv = TensorsView(s1,s2,et)
+    tv.setOrientation(TensorsView.Orientation.X1DOWN_X2RIGHT)
+    tv.setLineColor(Color.YELLOW)
+    tv.setLineWidth(3.0)
+    tv.setScale(2.0)
+    panel.getTile(0,0).addTiledView(tv)
+  else:
+    fs,x1s,x2s = makePointSets(cmap,f,x1,x2)
+    for i in range(len(fs)):
+      color = cmap.getColor(fs[i][0])
+      color = Color(color.red,color.green,color.blue)
+      pv = panel.addPoints(x1s[i],x2s[i])
+      pv.setLineStyle(PointsView.Line.NONE)
+      pv.setMarkStyle(PointsView.Mark.FILLED_CIRCLE)
+      pv.setMarkSize(10)
+      pv.setMarkColor(color)
+  frame2Teapot(panel,png)
+
+def makePointSets(cmap,f,x1,x2):
+  sets = {}
+  for i in range(len(f)):
+    if f[i] in sets:
+      points = sets[f[i]]
+      points[0].append(f[i])
+      points[1].append(x1[i])
+      points[2].append(x2[i])
+    else:
+      points = [[f[i]],[x1[i]],[x2[i]]] # lists of f, x1, x2
+      sets[f[i]] = points
+  ns = len(sets)
+  fs = zerofloat(1,ns)
+  x1s = zerofloat(1,ns)
+  x2s = zerofloat(1,ns)
+  il = 0
+  for points in sets:
+    fl = sets[points][0]
+    x1l = sets[points][1]
+    x2l = sets[points][2]
+    nl = len(fl)
+    fs[il] = zerofloat(nl)
+    x1s[il] = zerofloat(nl)
+    x2s[il] = zerofloat(nl)
+    copy(fl,fs[il])
+    copy(x1l,x1s[il])
+    copy(x2l,x2s[il])
+    il += 1
+  return fs,x1s,x2s
+
+def panel2Teapot():
+  panel = PlotPanel(1,1,
+    PlotPanel.Orientation.X1DOWN_X2RIGHT,
+    PlotPanel.AxesPlacement.NONE)
+  return panel
+
+def frame2Teapot(panel,png=None):
+  frame = PlotFrame(panel)
+  frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+  frame.setFontSizeForPrint(8,240)
+  #frame.setSize(1240,774)
+  #frame.setFontSizeForSlide(1.0,0.8)
+  frame.setSize(1290,777)
+  frame.setVisible(True)
+  if png and pngDir:
+    frame.paintToPng(720,3.33,pngDir+"/"+png+".png")
+  return frame
+
+def plot2(f,x1,x2,g,s1,s2,title=None,png=None,contours=True,points=True):
+  n1 = len(g[0])
+  n2 = len(g)
+  panel = panel2()
+  panel.setHLimits(-5.0,323.0)
+  panel.setVLimits(-5.0,323.0)
+  panel.setHInterval(100.0)
+  panel.setVInterval(100.0)
+  panel.setHLabel("Easting (ft)")
+  panel.setVLabel("Northing (ft)")
+  panel.addColorBar("Elevation (ft)")
+  panel.setColorBarWidthMinimum(65)
+  pv = panel.addPixels(s1,s2,g)
+  pv.setClips(min(f),max(f))
+  pv.setInterpolation(PixelsView.Interpolation.NEAREST)
+  if points:
+    pv.setColorModel(cjet)
+  else:
+    pv.setColorModel(ajet)
+  if contours:
+    cv = panel.addContours(s1,s2,g)
+    cv.setContours(Sampling(10,25.0,700.0)) # 700 - 925
+    cv.setLineColor(Color.BLACK)
+  if points:
+    pv = panel.addPoints(x1,x2)
+    pv.setLineStyle(PointsView.Line.NONE)
+    pv.setMarkStyle(PointsView.Mark.FILLED_CIRCLE)
+    pv.setMarkSize(6)
+  frame2(panel,title,png)
+
+def panel2():
+  panel = PlotPanel(1,1,
+    PlotPanel.Orientation.X1RIGHT_X2UP,
+    PlotPanel.AxesPlacement.LEFT_BOTTOM)
+  return panel
+
+def frame2(panel,title=None,png=None):
+  frame = PlotFrame(panel)
+  frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+  if title:
+    panel.setTitle(title)
+    frame.setFontSize(16)
+    frame.setSize(1100,960)
+  else:
+    frame.setFontSize(16)
+    frame.setSize(1170,897)
+  frame.setVisible(True)
+  if png and pngDir:
+    frame.paintToPng(720,3.33,pngDir+"/"+png+".png")
+  return frame
+
+def plotSurface(f,x1,x2,g,s1,s2,png=None):
+  g = transpose(g)
+  n = len(f)
+  #f = mul(0.5,f)
+  #g = mul(0.5,g)
+  xyz = zerofloat(3*n)
+  copy(n,0,1,x1,0,3,xyz)
+  copy(n,0,1,x2,1,3,xyz)
+  copy(n,0,1, f,2,3,xyz)
+  tg = TriangleGroup(True,s1,s2,g)
+  pg = PointGroup(s1.delta*s1.count/100.0,xyz);
+  pg.setStates(StateSet.forTwoSidedShinySurface(Color.RED))
+  sf = SimpleFrame()
+  sf.orbitView.setScale(1.2)
+  sf.orbitView.setAxesOrientation(AxesOrientation.XOUT_YRIGHT_ZUP)
+  sf.setWorldSphere(s1.first,s2.first,min(f),s1.last,s2.last,max(f))
+  sf.world.addChild(tg)
+  sf.world.addChild(pg)
+  sf.setSize(1000,1000)
+  if png and pngDir:
+    sf.paintToFile(pngDir+png+".png")
+
+#############################################################################
+# Run everything on Swing thread.
+class RunMain(Runnable):
+  def run(self):
+    main(sys.argv)
+SwingUtilities.invokeLater(RunMain())
